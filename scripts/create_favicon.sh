a0a8731cdc606d73d773f2fc61f6899ca302256a
Alessandro Finamore <finale80@gmail.com>
2021-08-07 16:03:38 +0200

    updated favicon

--- scripts/create_favicon.sh
+++ scripts/create_favicon.sh
@@ -1,8 +1,13 @@
 #reference link https://graphicdesign.stackexchange.com/questions/77359/how-to-convert-a-square-svg-to-all-size-ico
 INKSCAPE=/Applications/Inkscape.app/Contents/MacOS/inkscape
 
-$INKSCAPE -w 16 -h 16 -e favicon16.png favicon.svg
-$INKSCAPE -w 32 -h 32 -e favicon32.png favicon.svg
-$INKSCAPE -w 48 -h 48 -e favicon48.png favicon.svg
+if [ -x $1 ]; then
+    echo "missing input file: $0 <favicon.svg>"
+    exit 1
+fi
+
+$INKSCAPE -w 16 -h 16 -o favicon16.png $1
+$INKSCAPE -w 32 -h 32 -o favicon32.png $1
+$INKSCAPE -w 48 -h 48 -o favicon48.png $1
 
 convert favicon16.png favicon32.png favicon48.png favicon.ico
