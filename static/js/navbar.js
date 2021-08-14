90a0e1a7870b2bb4118f35c3d3e757760e11bb22
Alessandro Finamore <finale80@gmail.com>
2021-08-14 15:44:59 +0200

    improved navbar

---
+++ static/js/navbar.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    $("#logo").addClass("logo-reducedsize");
+    $("#navbar-right").addClass("navbar-right-reducedsize");
+    //document.getElementById("logo").style.height = "25px";
+    //document.getElementById("navbar-right").style.height = "35px";
+    //document.getElementById("navbar-right").style.lineHeight = "35px";
+  } else {
+    $("#logo").removeClass("logo-reducedsize");
+    $("#navbar-right").removeClass("navbar-right-reducedsize");
+    //document.getElementById("logo").style.height = "50px";
+    //document.getElementById("navbar-right").style.height = "60px";
+    //document.getElementById("navbar-right").style.lineHeight = "60px";
+  }
+}
