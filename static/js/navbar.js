37e1d56ef431365d52658db2ee9d4dc4f194622b
Alessandro Finamore <finale80@gmail.com>
2021-08-15 16:54:12 +0200

    integrated avatar in navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -4,12 +4,14 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     $("#logo").addClass("logo-reducedsize");
     $("#navbar-right").addClass("navbar-right-reducedsize");
+    $("#avatar").addClass("hidden");
     //document.getElementById("logo").style.height = "25px";
     //document.getElementById("navbar-right").style.height = "35px";
     //document.getElementById("navbar-right").style.lineHeight = "35px";
   } else {
     $("#logo").removeClass("logo-reducedsize");
     $("#navbar-right").removeClass("navbar-right-reducedsize");
+    $("#avatar").removeClass("hidden");
     //document.getElementById("logo").style.height = "50px";
     //document.getElementById("navbar-right").style.height = "60px";
     //document.getElementById("navbar-right").style.lineHeight = "60px";
