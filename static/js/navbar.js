4088ab2c7697dae87666f04106194083f9b97f4e
Alessandro Finamore <finale80@gmail.com>
2021-08-19 16:17:02 +0200

    fixed navbar in single page layout

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -25,40 +25,52 @@ var sections = [];
 var navItems = {};
 
 for (secID of sectionIDs) {
-    sections.push(document.querySelector("#" + secID));
-    navItems[secID] = document.querySelector("#navbar-link-" + secID);
+    sec = document.querySelector("#" + secID);
+    navItem = document.querySelector("#navbar-link-" + secID);
+
+    if (sec == null || navItem == null) {
+        sections = [];
+        navItems = {};
+        break;
+    }
+
+    sections.push(sec);
+    navItems[secID] = navItem;
 }
 
+if (sections.length > 0) {
 
-// intersection observer setup
-const observerOptions = {
-  root: null,
-  rootMargin: '0px',
-  threshold: [0.5, 0.35, 0.5, 0.5],
-};
+    // intersection observer setup
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: [0.5, 0.35, 0.5, 0.5],
+    };
 
-function observerCallback(entries, observer) {
+    function observerCallback(entries, observer) {
 
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      // get the nav item corresponding to the id of the section
-      // that is currently in view
-      const navItem = navItems[entry.target.id];
-      //navItem.focus();
-      // add 'active' class on the navItem
-      navItem.classList.add('navbar-link-active');
-      // remove 'active' class from any navItem that is not
-      // same as 'navItem' defined above
-      Object.values(navItems).forEach((item) => {
-        if (item != navItem) {
-          item.classList.remove('navbar-link-active');
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // get the nav item corresponding to the id of the section
+          // that is currently in view
+          const navItem = navItems[entry.target.id];
+          //navItem.focus();
+          // add 'active' class on the navItem
+          navItem.classList.add('navbar-link-active');
+          // remove 'active' class from any navItem that is not
+          // same as 'navItem' defined above
+          Object.values(navItems).forEach((item) => {
+            if (item != navItem) {
+              item.classList.remove('navbar-link-active');
+            }
+          });
         }
       });
     }
-  });
+
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    sections.forEach((sec) => observer.observe(sec));
+
 }
-
-
-const observer = new IntersectionObserver(observerCallback, observerOptions);
-
-sections.forEach((sec) => observer.observe(sec));
